e1f61bf004ae778d737ce35c31920cbb3e34ba58
Tarun <tarunkumar23j@gmail.com>
2024-12-15 08:11:47 +0530

    Basic routing setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import AdminPanel from './pages/AdminPanel'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
-    <div>Hello</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/admin-panel' element={<AdminPanel />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </div>
   )
 }
 
