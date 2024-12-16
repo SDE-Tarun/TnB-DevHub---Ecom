0ad37facbb8360b559f2f226b2bb63c5fc21dbfa
Tarun <tarunkumar23j@gmail.com>
2024-12-16 19:54:23 +0530

    Login and Signup Page created and Tailwind CSS is setup

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setemail] = useState("");
 
-export default Login
+  const [password, setpassword] = useState("");
+
+  const [userData, setuserData] = useState({});
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setuserData({
+      email: email,
+      password: password,
+    });
+    setemail("");
+    setpassword("");
+    console.log(userData);
+  };
+
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+      >
+        <label for="email">Email:</label>
+        <br />
+        <input
+          value={email}
+          onChange={(e) => {
+            setemail(e.target.value);
+          }}
+          type="email"
+          name="email"
+          id="email"
+        />
+        <br />
+        <label for="password">Password:</label>
+        <br />
+        <input
+          value={password}
+          onChange={(e) => {
+            setpassword(e.target.value);
+          }}
+          type="password"
+          name="password"
+          id="password"
+        />
+        <br />
+        <br />
+        <button>Login</button>
+        <br />
+        <p>
+          New here ? <a href="/signup">Create User</a>
+        </p>
+      </form>
+    </>
+  );
+};
+
+export default Login;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [name, setname] = useState("");
 
-export default Signup
+  const [email, setemail] = useState("");
+
+  const [password, setpassword] = useState("");
+
+  const [userData, setuserData] = useState({});
+
+  const submitHandler = (e) => {
+   e.preventDefault();
+   setuserData({
+    name: name,
+    email: email,
+    password: password
+   })
+   setname('');
+   setemail('');
+   setpassword('')
+   console.log(userData);
+   
+  }
+
+  return (
+    <>
+      <form onSubmit={(e)=> {
+        submitHandler(e);
+      }}>
+        <label for="name">Name:</label>
+        <br />
+        <input
+          value={name}
+          onChange={(e) => {
+            setname(e.target.value);
+          }}
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Enter your name"
+        />
+        <br />
+        <br />
+        <label for="email">Email:</label>
+        <br />
+        <input
+          value={email}
+          onChange={(e) => {
+            setemail(e.target.value);
+          }}
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Enter your email"
+        />
+        <br />
+        <br />
+        <label for="password">Password:</label>
+        <br />
+        <input
+          value={password}
+          onChange={(e) => {
+            setpassword(e.target.value);
+          }}
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Enter your password"
+        />
+        <br />
+        <br />
+        <button>Signup</button>
+        <br />
+        <p>
+          Already a user ? <a href="/login">Login Here</a>
+        </p>
+      </form>
+    </>
+  );
+};
+
+export default Signup;
