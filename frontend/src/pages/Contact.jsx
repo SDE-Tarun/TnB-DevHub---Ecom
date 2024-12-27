a50404b767f124adca54ca452a170d87de552030
Tarun <tarunkumar23j@gmail.com>
2024-12-27 11:34:21 +0530

    Commented code removed and code formatted of pages folder

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,63 +1,3 @@
-// import { motion } from "framer-motion";
-// import mainImg from "../assets/contact_us.gif";
-// import DescribedImage from "../components/DescribedImage";
-// import HeaderDashed from "../components/HeaderDashed";
-
-// const Contact = () => {
-//   return (
-//     <motion.div
-//       initial={{ opacity: 0 }}
-//       animate={{ opacity: 1 }}
-//       exit={{ opacity: 0 }}
-//       className="contact-page text-center py-3 pt-5"
-//     >
-//       <div className="container">
-//         {/* Header with dashed style */}
-//         <HeaderDashed head1="CONTACT" head2="US" classStyle="fw-normal fs-3" />
-
-//         {/* Described Image Section */}
-//         <DescribedImage
-//           img={mainImg}
-//           imgTitle="desk image"
-//           styleInLarge="justify-content-center column-gap-xl-4"
-//           styleImg="col-xl-5"
-//           styleText="col-xl-5"
-//           sideText={
-//             <>
-//               {/* Store Information */}
-//               <div className="our-store">
-//                 <h3 className="c-d-gray">Our Store</h3>
-//                 <address className="my-4">
-//                   <span>First Address</span>
-//                   <br />
-//                   <span>Second Address</span>
-//                 </address>
-//                 <div className="telephone">
-//                   Tel: Number Here
-//                   <br />
-//                   Email: Email Here
-//                 </div>
-//               </div>
-//               {/* Careers Information */}
-//               <div className="careers mt-5">
-//                 <h4 className="c-d-gray">Careers at Algohary Shop</h4>
-//                 <span className="d-block my-4">
-//                   Learn more about our teams and job openings.
-//                 </span>
-//                 <button className="btn py-3 px-4 border-out-d-gray rounded-0">
-//                   Explore Jobs
-//                 </button>
-//               </div>
-//             </>
-//           }
-//         />
-//       </div>
-//     </motion.div>
-//   );
-// };
-
-// export default Contact;
-
 import { motion } from "framer-motion";
 import mainImg from "../assets/contact_img.png";
 import DescribedImage from "../components/DescribedImage";
@@ -95,7 +35,11 @@ const Contact = () => {
       >
         {/* Header with dashed style */}
         <motion.div variants={fadeInUp}>
-          <HeaderDashed head1="CONTACT" head2="US" classStyle="fw-normal fs-3" />
+          <HeaderDashed
+            head1="CONTACT"
+            head2="US"
+            classStyle="fw-normal fs-3"
+          />
         </motion.div>
 
         {/* Described Image Section */}
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,220 +1,3 @@
-// // import React, { useState } from "react";
-
-// // import { useNavigate } from "react-router-dom";
-
-// // import { UserDataContext } from "../context/UserContext";
-
-// // import axios from "axios";
-
-// // const Login = () => {
-// //   const [username, setusername] = useState("");
-
-// //   const [password, setpassword] = useState("");
-
-// //   const [userData, setuserData] = useState({});
-
-// //   const { user, setuser } = React.useContext(UserDataContext);
-
-// //   const navigate = useNavigate();
-
-// //   const submitHandler = async (e) => {
-// //     e.preventDefault();
-
-// //     const user = {
-// //       username: username,
-// //       password: password,
-// //     };
-
-// //     const response = await axios.post(
-// //       `${import.meta.env.VITE_BASE_URL}/api/auth/login`,
-// //       user
-// //     );
-
-// //     if (response.status === 200) {
-// //       const data = response.data;
-// //       setuser(data.user);
-// //       navigate("/");
-// //     }
-
-// //     setusername("");
-// //     setpassword("");
-// //   };
-
-// //   return (
-// //     <>
-// //       <form
-// //         onSubmit={(e) => {
-// //           submitHandler(e);
-// //         }}
-// //       >
-// //         <label for="username">Username:</label>
-// //         <br />
-// //         <input
-// //           value={username}
-// //           onChange={(e) => {
-// //             setusername(e.target.value);
-// //           }}
-// //           type="text"
-// //           name="username"
-// //           id="username"
-// //         />
-// //         <br />
-// //         <label for="password">Password:</label>
-// //         <br />
-// //         <input
-// //           value={password}
-// //           onChange={(e) => {
-// //             setpassword(e.target.value);
-// //           }}
-// //           type="password"
-// //           name="password"
-// //           id="password"
-// //         />
-// //         <br />
-// //         <br />
-// //         <button>Login</button>
-// //         <br />
-// //         <p>
-// //           New here ? <a href="/signup">Create User</a>
-// //         </p>
-// //       </form>
-// //     </>
-// //   );
-// // };
-
-// // export default Login;
-
-// import { useState } from "react";
-// import { Formik, Form, Field, ErrorMessage } from "formik";
-// import { motion } from "framer-motion";
-// import HeaderDashed from "../components/HeaderDashed";
-// import { useNavigate, Link } from "react-router-dom";
-
-// const Login = () => {
-//   const [showPassword, setShowPassword] = useState(false);
-//   const [serverErrors, setServerErrors] = useState([]);
-//   const navigate = useNavigate();
-
-//   const handleSubmit = async (values) => {
-//     // Reset any previous errors
-//     setServerErrors([]);
-
-//     try {
-//       const response = await fetch("http://localhost:3000/api/auth/login", {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify(values),
-//       });
-//       console.log(response);
-
-//       if (response.ok) {
-//         const data = await response.json();
-//         console.log("Login successful", data);
-//         alert("User logged in successfully");
-//         navigate("/");
-//         // Handle successful login here (e.g., redirect)
-//       } else {
-//         const data = await response.json();
-//         setServerErrors(data.errors || []);
-//         alert("Credentials do not Match");
-//       }
-//     } catch (error) {
-//       console.error("Error during login:", error);
-//     }
-//   };
-
-//   return (
-//     <motion.div
-//       initial={{ opacity: 0 }}
-//       animate={{ opacity: 1 }}
-//       exit={{ opacity: 0 }}
-//       className="Login-Page text-center sec-padd"
-//     >
-//       <div className="container">
-//         <HeaderDashed head1="Log" head2="IN" />
-
-//         <Formik
-//           initialValues={{ username: "", password: "" }}
-//           onSubmit={handleSubmit}
-//         >
-//           {({ isSubmitting }) => (
-//             <Form className="mt-5 d-flex flex-column gap-5 align-items-center border border-2 p-4">
-//               <div className="d-flex flex-column align-items-start w-100">
-//                 <label htmlFor="username" className="mb-2 fs-4">
-//                   Username:
-//                 </label>
-//                 <Field
-//                   className="p-3 py-203 outline-0 w-100 border-gray border-05"
-//                   name="username"
-//                   type="text"
-//                   id="username"
-//                   placeholder="example@gmail.com"
-//                 />
-//                 <ErrorMessage
-//                   name="username"
-//                   component="div"
-//                   className="text-danger mt-2"
-//                 />
-//               </div>
-//               <div className="d-flex flex-column align-items-start w-100">
-//                 <label
-//                   htmlFor="password"
-//                   className="mb-2 fs-4 w-100 d-flex justify-content-between"
-//                 >
-//                   Password:{" "}
-//                   <span
-//                     className={`cursor c-gray ${showPassword && "active"}`}
-//                     onClick={() => setShowPassword((prev) => !prev)}
-//                   >
-//                     show
-//                   </span>
-//                 </label>
-//                 <Field
-//                   className="p-3 py-203 outline-0 w-100 border-gray border-05"
-//                   name="password"
-//                   type={`${showPassword ? "text" : "password"}`}
-//                   id="password"
-//                   placeholder="Enter Your Password"
-//                 />
-//                 <ErrorMessage
-//                   name="password"
-//                   component="div"
-//                   className="text-danger mt-2"
-//                 />
-//               </div>
-
-//               {/* Displaying server-side errors */}
-//               {serverErrors.length > 0 && (
-//                 <div className="text-danger mt-3">
-//                   {serverErrors.map((error, index) => (
-//                     <p key={index}>{error.msg}</p>
-//                   ))}
-//                 </div>
-//               )}
-
-//               <button
-//                 className="btn bg-black py-2 px-4 rounded c-white fs-5"
-//                 type="submit"
-//                 disabled={isSubmitting}
-//               >
-//                 Sign In
-//               </button>
-//               <p className="mt-4">
-//                 New here?{" "}
-//                 <Link to="/signup" className="text-primary">
-//                   Create account
-//                 </Link>
-//               </p>
-//             </Form>
-//           )}
-//         </Formik>
-//       </div>
-//     </motion.div>
-//   );
-// };
-
-// export default Login;
-
 import { useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { motion } from "framer-motion";
@@ -242,7 +25,7 @@ const Login = () => {
         navigate("/");
       } else {
         const data = await response.json();
-        alert('Please signup first');
+        alert("Please signup first");
         setServerErrors(data.errors || []);
       }
     } catch (error) {
@@ -355,41 +138,48 @@ const Login = () => {
                 Log In
               </motion.button>
 
-              
-<button
-  class="cursor-pointer text-black flex gap-2 items-center bg-white px-4 py-2 rounded-lg font-medium text-lg hover:bg-zinc-300 transition-all ease-in duration-200"
->
-  <svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" class="w-6">
-    <path
-      d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
-      fill="#FFC107"
-    ></path>
-    <path
-      d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
-      fill="#FF3D00"
-    ></path>
-    <path
-      d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
-      fill="#4CAF50"
-    ></path>
-    <path
-      d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
-      fill="#1976D2"
-    ></path>
-  </svg>
-  Continue with Google
-</button>
+              <button class="cursor-pointer text-black flex gap-2 items-center bg-white px-4 py-2 rounded-lg font-medium text-lg hover:bg-zinc-300 transition-all ease-in duration-200">
+                <svg
+                  viewBox="0 0 48 48"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-6"
+                >
+                  <path
+                    d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+                    fill="#FFC107"
+                  ></path>
+                  <path
+                    d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+                    fill="#FF3D00"
+                  ></path>
+                  <path
+                    d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+                    fill="#4CAF50"
+                  ></path>
+                  <path
+                    d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+                    fill="#1976D2"
+                  ></path>
+                </svg>
+                Continue with Google
+              </button>
 
-<button
-  class="cursor-pointer text-black flex gap-2 items-center bg-white px-4 py-2 rounded-lg font-medium text-lg hover:bg-zinc-300 transition-all ease-in duration-200"
->
-  {/* <!-- New Mobile Phone Icon --> */}
-  <svg xmlns="http://www.w3.org/2000/svg" class="w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-    <path d="M6 2H18C18.552 2 19 2.448 19 3V21C19 21.552 18.552 22 18 22H6C5.448 22 5 21.552 5 21V3C5 2.448 5.448 2 6 2ZM12 18C13.104 18 14 17.104 14 16C14 14.896 13.104 14 12 14C10.896 14 10 14.896 10 16C10 17.104 10.896 18 12 18ZM7 3H17V19H7V3Z"></path>
-  </svg>
-  Continue with Mobile Number
-</button>
-
+              <button class="cursor-pointer text-black flex gap-2 items-center bg-white px-4 py-2 rounded-lg font-medium text-lg hover:bg-zinc-300 transition-all ease-in duration-200">
+                {/* <!-- New Mobile Phone Icon --> */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-6"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M6 2H18C18.552 2 19 2.448 19 3V21C19 21.552 18.552 22 18 22H6C5.448 22 5 21.552 5 21V3C5 2.448 5.448 2 6 2ZM12 18C13.104 18 14 17.104 14 16C14 14.896 13.104 14 12 14C10.896 14 10 14.896 10 16C10 17.104 10.896 18 12 18ZM7 3H17V19H7V3Z"></path>
+                </svg>
+                Continue with Mobile Number
+              </button>
 
               <motion.p
                 className="mt-4"
@@ -412,4 +202,3 @@ const Login = () => {
 };
 
 export default Login;
-
