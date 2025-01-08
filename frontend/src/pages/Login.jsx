41575732ada679bd531d89f30b6b4fd415eeeeac
Tarun <tarunkumar23j@gmail.com>
2025-01-08 12:08:45 +0530

    Removed googleauth and mobileotp

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { motion } from "framer-motion";
 import HeaderDashed from "../components/HeaderDashed";
@@ -12,79 +12,79 @@ const Login = () => {
   const navigate = useNavigate();
   const { login } = useAuth();
 
-  useEffect(() => {
-    console.log('useefect block entered');
+  // useEffect(() => {
+  //   console.log('useefect block entered');
     
-    const handleCallback = async () => {
-      console.log('handlecallback function block entered');
+  //   const handleCallback = async () => {
+  //     console.log('handlecallback function block entered');
       
-      const params = new URLSearchParams(window.location.search);
-      console.log(params);
+  //     const params = new URLSearchParams(window.location.search);
+  //     console.log(params);
 
-      const code = params.get("code");
-      console.log(code);
+  //     const code = params.get("code");
+  //     console.log(code);
 
-      if (code) {
-        console.log('code is available');
-        try {
-          console.log('try block entered');
+  //     if (code) {
+  //       console.log('code is available');
+  //       try {
+  //         console.log('try block entered');
           
-          const response = await fetch(
-            `http://localhost:3000/api/auth/google/callback?code=${code}`
-          );
-          console.log(response);
+  //         const response = await fetch(
+  //           `http://localhost:3000/api/auth/google/callback?code=${code}`
+  //         );
+  //         console.log(response);
           
-          const data = await response.json();
-          console.log(data);
+  //         const data = await response.json();
+  //         console.log(data);
           
 
-          if (data.token && data.user) {
-            console.log('if block of token entered');
+  //         if (data.token && data.user) {
+  //           console.log('if block of token entered');
             
-            localStorage.setItem("googleToken", data.token);
-            localStorage.setItem("user", JSON.stringify(data.user));
-            // Update the AuthContext with the user data
-            login(data.user);
-            toast.success("Google login successful");
-            window.location.href = "/"; // Redirect to homepage
-            // navigate('/');
-          } else {
-            toast.error("Google login failed");
-          }
-        } catch (error) {
-          console.log(error, "Error in google login callback");
-          toast.error("Something went wrong. Please try again!"); // Show generic error toast
-        }
-      }
-    };
-    handleCallback();
-  }, [navigate, login]);
+  //           localStorage.setItem("googleToken", data.token);
+  //           localStorage.setItem("user", JSON.stringify(data.user));
+  //           // Update the AuthContext with the user data
+  //           login(data.user);
+  //           toast.success("Google login successful");
+  //           window.location.href = "/"; // Redirect to homepage
+  //           // navigate('/');
+  //         } else {
+  //           toast.error("Google login failed");
+  //         }
+  //       } catch (error) {
+  //         console.log(error, "Error in google login callback");
+  //         toast.error("Something went wrong. Please try again!"); // Show generic error toast
+  //       }
+  //     }
+  //   };
+  //   handleCallback();
+  // }, [navigate, login]);
 
-  const handleGoogleLogin = async () => {
-    console.log('handleGooglelogin function block entered');
+  // const handleGoogleLogin = async () => {
+  //   console.log('handleGooglelogin function block entered');
     
-    try {
-      console.log('Entered try block of google login');
+  //   try {
+  //     console.log('Entered try block of google login');
       
-      const response = await fetch(
-        "http://localhost:3000/api/auth/google/login"
-      );
-      console.log(response);
+  //     const response = await fetch(
+  //       "http://localhost:3000/api/auth/google/login"
+  //     );
+  //     console.log(response);
       
-      const data = await response.json();
-      console.log(data);
+  //     const data = await response.json();
+  //     console.log(data);
       
-      if (data.authUrl) {
-        console.log('Google login url obtained');
-        window.location.href = data.authUrl; // Redirect to Google Auth
-      } else {
-        toast.error("Failed to login with Google");
-      }
-    } catch (error) {
-      console.error("Error fetching auth URL:", error);
-      toast.error("Something went wrong. Please try again!"); // Show generic error toast
-    }
-  };
+  //     if (data.authUrl) {
+  //       console.log('Google login url obtained');
+  //       window.location.href = data.authUrl; // Redirect to Google Auth
+  //     } else {
+  //       toast.error("Failed to login with Google");
+  //     }
+  //   } catch (error) {
+  //     console.error("Error fetching auth URL:", error);
+  //     toast.error("Something went wrong. Please try again!"); // Show generic error toast
+  //   }
+  // };
 
   const handleSubmit = async (values) => {
     setServerErrors([]);
@@ -218,7 +218,7 @@ const Login = () => {
                 Log In
               </motion.button>
 
-              <button
+              {/* <button
                 type="button"
                 onClick={handleGoogleLogin}
                 class="cursor-pointer text-black flex gap-2 items-center bg-white px-4 py-2 rounded-lg font-medium text-medium hover:bg-zinc-300 transition-all ease-in duration-200"
@@ -246,10 +246,21 @@ const Login = () => {
                   ></path>
                 </svg>
                 Continue with Google
-              </button>
+              </button> */}
 
+{/* <button class="button" type="button" onClick={handleGoogleLogin}>
+  <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" viewBox="0 0 256 262" class="svg">
+  <path fill="#4285F4" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027" class="blue"></path>
+  <path fill="#34A853" d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1" class="green"></path>
+  <path fill="#FBBC05" d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782" class="yellow"></path>
+  <path fill="#EB4335" d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251" class="red"></path>
+</svg>
+<span class="text">Continue with Google</span>
+</button> */}
+
+{/* 
               <button class="cursor-pointer text-black flex gap-2 items-center bg-white px-4 py-2 rounded-lg font-medium text-medium hover:bg-zinc-300 transition-all ease-in duration-200">
-                {/* <!-- New Mobile Phone Icon --> */}
+                
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   class="w-6"
@@ -263,7 +274,7 @@ const Login = () => {
                   <path d="M6 2H18C18.552 2 19 2.448 19 3V21C19 21.552 18.552 22 18 22H6C5.448 22 5 21.552 5 21V3C5 2.448 5.448 2 6 2ZM12 18C13.104 18 14 17.104 14 16C14 14.896 13.104 14 12 14C10.896 14 10 14.896 10 16C10 17.104 10.896 18 12 18ZM7 3H17V19H7V3Z"></path>
                 </svg>
                 Continue with Mobile Number
-              </button>
+              </button> */}
 
               <motion.p
                 className="mt-4"
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -165,7 +165,7 @@ const Signup = () => {
                 Create Account
               </motion.button>
 
-              <button class="cursor-pointer text-black flex gap-2 items-center bg-white px-4 py-2 rounded-lg font-medium text-medium hover:bg-zinc-300 transition-all ease-in duration-200">
+              {/* <button class="cursor-pointer text-black flex gap-2 items-center bg-white px-4 py-2 rounded-lg font-medium text-medium hover:bg-zinc-300 transition-all ease-in duration-200">
                 <svg
                   viewBox="0 0 48 48"
                   xmlns="http://www.w3.org/2000/svg"
@@ -189,10 +189,19 @@ const Signup = () => {
                   ></path>
                 </svg>
                 Continue with Google
-              </button>
+              </button> */}
 
-              <button class="cursor-pointer text-black flex gap-2 items-center bg-white px-4 py-2 rounded-lg font-medium text-medium hover:bg-zinc-300 transition-all ease-in duration-200">
-                {/* <!-- New Mobile Phone Icon --> */}
+{/* <button class="button" type="button">
+  <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" viewBox="0 0 256 262" class="svg">
+  <path fill="#4285F4" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027" class="blue"></path>
+  <path fill="#34A853" d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1" class="green"></path>
+  <path fill="#FBBC05" d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782" class="yellow"></path>
+  <path fill="#EB4335" d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251" class="red"></path>
+</svg>
+<span class="text">Continue with Google</span>
+</button> */}
+
+              {/* <button class="cursor-pointer text-black flex gap-2 items-center bg-white px-4 py-2 rounded-lg font-medium text-medium hover:bg-zinc-300 transition-all ease-in duration-200">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   class="w-6"
@@ -206,7 +215,7 @@ const Signup = () => {
                   <path d="M6 2H18C18.552 2 19 2.448 19 3V21C19 21.552 18.552 22 18 22H6C5.448 22 5 21.552 5 21V3C5 2.448 5.448 2 6 2ZM12 18C13.104 18 14 17.104 14 16C14 14.896 13.104 14 12 14C10.896 14 10 14.896 10 16C10 17.104 10.896 18 12 18ZM7 3H17V19H7V3Z"></path>
                 </svg>
                 Continue with Mobile Number
-              </button>
+              </button> */}
 
               <motion.p
                 initial={{ opacity: 0 }}
