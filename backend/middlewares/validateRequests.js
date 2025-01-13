db0db388562de7776bbfafed131bc8f737cb7f46
Bhaskar-ranawat <bhaskarshekhar6500@gmail.com>
2025-01-13 11:40:09 +0530

    Product management routes are completed and unit tested

--- backend/middlewares/validateRequests.js
+++ backend/middlewares/validateRequests.js
@@ -1,6 +1,8 @@
 const validateRequest = (schema) => (req, res, next) => {
-  const { error, value } = schema.validate(req.body);
+  const { _id, ...productData } = req.body;
+  const { error, value } = schema.validate(productData);
   if (error) {
+    console.log(error);
     return res.status(400).json({ message: error.details[0].message });
   }
   req.validatedBody = value;
