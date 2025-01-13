aca03c1e1c94f3875f0e2469cffe000e3ceac93d
Tarun <tarunkumar23j@gmail.com>
2025-01-13 16:29:18 +0530

    Contact Page Image Added

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -246,7 +246,7 @@ const Contact = () => {
       <motion.div
         className="about-banner"
         style={{
-          backgroundImage: `url('https://images.unsplash.com/photo-1487611459768-bd414656ea10?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDMxfHx8ZW58MHx8fHx8')`,
+          backgroundImage: `url('https://images.unsplash.com/photo-1512486130939-2c4f79935e4f?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           minHeight: 'calc(100vh - 2rem)', // Set height of the banner
